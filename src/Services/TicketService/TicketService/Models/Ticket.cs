23e1a4c837c407515be1b0ce4b1f2c1828151f93
charles <charles.claude@epitech.eu>
2020-04-22 13:15:01 +0200

    Ticket Micro service wip

---
+++ src/Services/TicketService/TicketService/Models/Ticket.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicketService.Models
+{
+    public class Ticket
+    {
+        public long Id { get; set; }
+    }
+}
---
+++ src/Services/TicketService/TicketService/Models/TicketContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TicketService.Models
+{
+    public class TicketContext : DbContext
+    {
+        public TicketContext(DbContextOptions<TicketContext> options) : base(options) { }
+
+        public DbSet<Ticket> Ticket { get; set; }
+    }
+}
