13a250f9c56b4711ffb11e5869e62100c0af0686
cirmnt <clement.royer@epitech.eu>
2020-04-23 00:29:49 +0200

    [FIX] Going deeper

--- run.bat
+++ run.bat
@@ -1,9 +1,9 @@
 cd src/APIGateway/APIGateway
 start cmd /k Call dotnet run
-cd ../Services/PaymentService/PaymentService/
+cd ../../Services/PaymentService/PaymentService/
 start cmd /k Call dotnet run
-cd ../TicketService/TicketService
+cd ../../TicketService/TicketService
 start cmd /k Call dotnet run
-cd ../UserServices/UsersServices
+cd ../../UserServices/UsersServices
 start cmd /k Call dotnet run
-cd ../../
+cd ../../../
