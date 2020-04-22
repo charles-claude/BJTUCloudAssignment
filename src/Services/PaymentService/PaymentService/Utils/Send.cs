07d2b25a4a083551b583c571ed2466f39676516f
cirmnt <clement.royer@epitech.eu>
2020-04-22 17:51:16 +0200

    am

---
+++ src/Services/PaymentService/PaymentService/Utils/Send.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+class Sender
+{
+    public static void Send(string queue, string data)
+    {
+        using (IConnection connection = new ConnectionFactory().CreateConnection())
+        {
+            using (IModel channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue, false, false, false, null);
+                channel.BasicPublish(string.Empty, queue, null, Encoding.UTF8.GetBytes(data));
+            }
+        }
+    }
+}
---
+++ src/Services/PaymentService/PaymentService/Utils/inutile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentService.Utils
+{
+    public class inutile
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
